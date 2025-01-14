044029af8abc40e63ce8e53ae7469a17e74ef16f
Abraham Sinsard <abrahamsinsard@MacBookPro.lan>
2025-01-14 10:00:32 +0100

    add swap functions

---
+++ src/move/swap.c
@@ -0,0 +1,34 @@
+#include "../../include/push_swap.h"
+
+void	swap_a(t_pile **a)
+{
+	int	tmp;
+	if ((*a)->value && (*a)->next->value)
+	{
+		tmp = (*a)->value;
+		(*a)->value = (*a)->next->value;
+		(*a)->next->value = tmp;
+	}
+	else
+		return ;
+}
+
+void	swap_a(t_pile **b)
+{
+	int	tmp;
+
+	if ((*b)->value && (*b)->next->value)
+	{
+		tmp = (*b)->value;
+		(*b)->value = (*b)->next->value;
+		(*b)->next->value = tmp;
+	}
+	else
+		return ;
+}
+
+void	swap_ss(t_pile **a, t_pile **b)
+{
+	swap_a(a);
+	swap_b(b);
+}
